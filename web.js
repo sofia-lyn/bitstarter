35747d0bc9db1e1a13095d311cc6b32776f463fe
sofia-lyn <sofia@sofialyn.com>
2013-07-22 08:16:53 +0000

    accessing index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('This is testing from sof');
+  response.send(fs.writeFileSync(index.html);
 });
 
 var port = process.env.PORT || 5000;
