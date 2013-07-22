835d481d905aa09e29401872fbaad4b91ee6fa38
sofia-lyn <sofia@sofialyn.com>
2013-07-22 08:56:14 +0000

    accessing index.thml 7

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
- response.readFileSync("index.html");
+ express.readFileSync("index.html");
 });
 
 var port = process.env.PORT || 5000;
