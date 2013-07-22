200a5913d72374d1c33a96774e75ef398e2a8145
sofia-lyn <sofia@sofialyn.com>
2013-07-22 09:00:23 +0000

    accessing index.thml 8

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
- express.readFileSync("index.html");
+ fs.readFileSync("index.html");
 });
 
 var port = process.env.PORT || 5000;
